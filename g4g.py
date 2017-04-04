d66c20cd9271f367d962919c71c7584db16c3ecf
CuriousLearner <sanyam.khurana01@gmail.com>
2017-04-04 16:11:42 +0530

    Refactors some other nits

--- g4g.py
+++ g4g.py
@@ -48,26 +48,29 @@ def get_category_choice():
 def save_articles_as_html_and_pdf():
     print("All links scraped, extracting articles")
     # Formatting the html for articles
-    all_articles = ('<!DOCTYPE html>'
-                    '<html><head>'
-                    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
-                    '<link rel="stylesheet" href="style.min.css" type="text/css" media="all" />'
-                    '<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>'
-                    '</head><body>'
-                    )
+    all_articles = (
+        '<!DOCTYPE html>'
+        '<html><head>'
+        '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
+        '<link rel="stylesheet" href="style.min.css" type="text/css" media="all" />'
+        '<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>'
+        '</head><body>'
+    )
+
     all_articles += '<h1 style="text-align:center;font-size:40px">' + categoryUrl.title() + ' Archive</h1><hr>'
     all_articles += '<h1 style="padding-left:5%;font-size:200%;">Index</h1><br/>'
+
     for x in range(len(articles)):
         all_articles += '<a href =\"#' + str(x+1) + '\">' + '<h1 style="padding-left:5%;font-size:20px;">' + str(x+1) + ".\t\t"+articles[x].title + '</h1></a> <br/>'
     for x in range(len(articles)):
         all_articles += '<hr id=\"' + str(x+1) +'\">' + articles[x].content.decode("utf-8")
-    all_articles += '''
-                    </body></html>
-                   '''
+
+    all_articles += '''</body></html>'''
     html_file_name = 'G4G_' + categoryUrl.title() + '.html'
     html_file = open(html_file_name, "w")
     html_file.write(all_articles.encode("utf-8"))
     html_file.close()
+
     pdf_file_name = 'G4G_' + categoryUrl.title() + '.pdf'
     print("Generating PDF " + pdf_file_name)
     html_to_pdf_command = 'wkhtmltopdf ' + html_file_name + ' ' + pdf_file_name
@@ -80,6 +83,7 @@ def scrape_category(category_url):
     except ConnectionError:
         print("Couldn't connect to Internet! Please check your connection & Try again.")
         exit(1)
+
     # Selecting links which are in the category page
     links = [a.attrs.get('href') for a in soup.select('article li a')]
     # Removing links for the categories with anchor on same page
