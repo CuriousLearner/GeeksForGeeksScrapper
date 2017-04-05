b89c4194e5dd9cc7405ce29a53f70b35b47ca17e
CuriousLearner <sanyam.khurana01@gmail.com>
2017-04-05 10:01:30 +0530

    More code clean up

--- g4g.py
+++ g4g.py
@@ -57,21 +57,24 @@ def save_articles_as_html_and_pdf():
         '</head><body>'
     )
 
-    all_articles += '<h1 style="text-align:center;font-size:40px">' + categoryUrl.title() + ' Archive</h1><hr>'
+    all_articles += '<h1 style="text-align:center;font-size:40px">' + \
+                    category_url.title() + ' Archive</h1><hr>'
     all_articles += '<h1 style="padding-left:5%;font-size:200%;">Index</h1><br/>'
 
     for x in range(len(articles)):
-        all_articles += '<a href =\"#' + str(x+1) + '\">' + '<h1 style="padding-left:5%;font-size:20px;">' + str(x+1) + ".\t\t"+articles[x].title + '</h1></a> <br/>'
+        all_articles += '<a href =\"#' + str(x+1) + '\">' + \
+                        '<h1 style="padding-left:5%;font-size:20px;">' + \
+                        str(x+1) + ".\t\t"+articles[x].title + '</h1></a> <br/>'
     for x in range(len(articles)):
         all_articles += '<hr id=\"' + str(x+1) +'\">' + articles[x].content.decode("utf-8")
 
     all_articles += '''</body></html>'''
-    html_file_name = 'G4G_' + categoryUrl.title() + '.html'
+    html_file_name = 'G4G_' + category_url.title() + '.html'
     html_file = open(html_file_name, "w")
     html_file.write(all_articles.encode("utf-8"))
     html_file.close()
 
-    pdf_file_name = 'G4G_' + categoryUrl.title() + '.pdf'
+    pdf_file_name = 'G4G_' + category_url.title() + '.pdf'
     print("Generating PDF " + pdf_file_name)
     html_to_pdf_command = 'wkhtmltopdf ' + html_file_name + ' ' + pdf_file_name
     system(html_to_pdf_command)
@@ -124,6 +127,6 @@ def scrape_category(category_url):
 
 if __name__ == '__main__':
     display_menu()
-    categoryUrl = get_category_choice()
-    scrape_category(categoryUrl)
+    category_url = get_category_choice()
+    scrape_category(category_url)
     save_articles_as_html_and_pdf()
