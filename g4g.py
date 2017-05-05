41e94510986caa89df174241a0907db7ef7646c6
Sanyam Khurana <CuriousLearner@users.noreply.github.com>
2017-05-05 17:46:13 +0530

    Minor code clean-up & beautification (#15)
    * Refactoring variable names + other PEP 8 recommendations
    
    * Refactors some other nits
    
    * More code clean up

--- g4g.py
+++ g4g.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
 
 import requests
-from bs4 import BeautifulSoup
+
 from os import system
 from sys import exit
 from time import sleep
 from requests.exceptions import ConnectionError
+
+from bs4 import BeautifulSoup
+
 from article import Article
+
 
 BASE_URL = 'http://www.geeksforgeeks.org/'
 articles = []
 
-choice_to_category = {1: 'c', 2: 'c-plus-plus', 3: 'java', 4: 'python',
-                      5: 'fundamentals-of-algorithms',
-                      6: 'data-structures'}
+CHOICE_TO_CATEGORY_MAPPING = {
+    1: 'c',
+    2: 'c-plus-plus',
+    3: 'java',
+    4: 'python',
+    5: 'fundamentals-of-algorithms',
+    6: 'data-structures'
+}
 
 
 def display_menu():
@@ -29,48 +38,55 @@ def display_menu():
 def get_category_choice():
     choice = int(raw_input("Enter choice: "))
     try:
-        categoryUrl = choice_to_category[choice]
+        category_url = CHOICE_TO_CATEGORY_MAPPING[choice]
     except KeyError:
         print("Wrong Choice Entered. Exiting!")
         exit(1)
-    return categoryUrl
+    return category_url
 
 
 def save_articles_as_html_and_pdf():
     print("All links scraped, extracting articles")
     # Formatting the html for articles
-    allArticles = ('<!DOCTYPE html>'
-                    '<html><head>'
-                    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
-                    '<link rel="stylesheet" href="style.min.css" type="text/css" media="all" />'
-                    '<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>'
-                    '</head><body>'
-                    )
-    allArticles += '<h1 style="text-align:center;font-size:40px">' + categoryUrl.title() + ' Archive</h1><hr>'
-    allArticles += '<h1 style="padding-left:5%;font-size:200%;"> Index</h1><Br/>'
+    all_articles = (
+        '<!DOCTYPE html>'
+        '<html><head>'
+        '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
+        '<link rel="stylesheet" href="style.min.css" type="text/css" media="all" />'
+        '<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>'
+        '</head><body>'
+    )
+
+    all_articles += '<h1 style="text-align:center;font-size:40px">' + \
+                    category_url.title() + ' Archive</h1><hr>'
+    all_articles += '<h1 style="padding-left:5%;font-size:200%;">Index</h1><br/>'
+
     for x in range(len(articles)):
-        allArticles += '<a href =\"#' + str(x+1) + '\">' + '<h1 style="padding-left:5%;font-size:20px;">' + str(x+1) + ".\t\t"+articles[x].title + '</h1></a> <br/>'
+        all_articles += '<a href =\"#' + str(x+1) + '\">' + \
+                        '<h1 style="padding-left:5%;font-size:20px;">' + \
+                        str(x+1) + ".\t\t"+articles[x].title + '</h1></a> <br/>'
     for x in range(len(articles)):
-        allArticles += '<hr id=\"' + str(x+1) +'\">' + articles[x].content.decode("utf-8")
-    allArticles += '''
-                    </body></html>
-                   '''
-    html_file_name = 'G4G_' + categoryUrl.title() + '.html'
-    Html_file = open(html_file_name, "w")
-    Html_file.write(allArticles.encode("utf-8"))
-    Html_file.close()
-    pdf_file_name = 'G4G_' + categoryUrl.title() + '.pdf'
+        all_articles += '<hr id=\"' + str(x+1) +'\">' + articles[x].content.decode("utf-8")
+
+    all_articles += '''</body></html>'''
+    html_file_name = 'G4G_' + category_url.title() + '.html'
+    html_file = open(html_file_name, "w")
+    html_file.write(all_articles.encode("utf-8"))
+    html_file.close()
+
+    pdf_file_name = 'G4G_' + category_url.title() + '.pdf'
     print("Generating PDF " + pdf_file_name)
     html_to_pdf_command = 'wkhtmltopdf ' + html_file_name + ' ' + pdf_file_name
     system(html_to_pdf_command)
 
 
-def scrape_category(categoryUrl):
+def scrape_category(category_url):
     try:
-        soup = BeautifulSoup(requests.get(BASE_URL + categoryUrl).text)
+        soup = BeautifulSoup(requests.get(BASE_URL + category_url).text)
     except ConnectionError:
         print("Couldn't connect to Internet! Please check your connection & Try again.")
         exit(1)
+
     # Selecting links which are in the category page
     links = [a.attrs.get('href') for a in soup.select('article li a')]
     # Removing links for the categories with anchor on same page
@@ -82,11 +98,11 @@ def scrape_category(categoryUrl):
     # Traverse each link to find article and save it.
     for link in links:
         try:
-            if(i % 10 == 0):
+            if i % 10 == 0:
                 sleep(5)  # Sleep for 5 seconds before scraping every 10th link
             link = link.strip()
             print("Scraping link no: " + str(i) + " Link: " + link)
-            i = i + 1
+            i += 1
             link_soup = BeautifulSoup(requests.get(link).text)
             # Remove the space occupied by Google Ads (Drop script & ins node)
             [script.extract() for script in link_soup(["script", "ins"])]
@@ -111,6 +127,6 @@ def scrape_category(categoryUrl):
 
 if __name__ == '__main__':
     display_menu()
-    categoryUrl = get_category_choice()
-    scrape_category(categoryUrl)
+    category_url = get_category_choice()
+    scrape_category(category_url)
     save_articles_as_html_and_pdf()
